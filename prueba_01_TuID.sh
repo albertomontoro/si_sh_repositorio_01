37b68073bd2fd3d1757b34438a8f31c7a761cfd3
A. Montoro <amontoro.sistemasinformaticos@gmail.com>
2022-05-12 20:28:07 +0200

    Creando un conflicto entre ramas

--- prueba_01_TuID.sh
+++ prueba_01_TuID.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 clear
-echo "Soy el script 01 con un conflicto en la misma línea"
+echo "Soy el script 01"
 #Main: Voy a crear un conflicto entre esta rama y la otra.
+echo "Soy el script 01 con un conflicto en la misma línea"
 exit 0
